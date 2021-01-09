5d2a059371b60ee360df9c869c5bd07c68fcc24b
jsandobalin <jsandobalin@gmail.com>
2021-01-08 21:43:48 -0500

    It is change in the calculator execute class.

--- src/main/java/ec/edu/epn/git/calculator/CalculatorExecute.java
+++ src/main/java/ec/edu/epn/git/calculator/CalculatorExecute.java
@@ -11,5 +11,7 @@ public class CalculatorExecute {
         int subtraction = c.subtraction(7,1);
         System.out.println("c.subtraction(7,1) = " + subtraction);
 
+        System.out.println("== It is a change !! ==");
+
     }
 }
